c19e2fb37015ce855038d085c3d2bb021e1c30e6
swygit <112494437+swygit@users.noreply.github.com>
2023-03-27 01:41:21 +0800

    Customer Profile
    Created Customer Profile page according to design template, added ability to upload images.

--- l2g4-econ-food/src/components/MerchantNavBar.vue
+++ l2g4-econ-food/src/components/MerchantNavBar.vue
@@ -2,17 +2,17 @@
     <div class="nav">
       <ul>
         <img class="image" src="https://i.ibb.co/JypV2p6/ECON-FOOD-LOGO.png" />
-        <li><a href="#">Profile</a></li>
-        <li><a href="#">Marketplace</a></li>
         <li><a href="#">Orders</a></li>
-        <li><a href="#">Cart</a></li>
+        <li><a href="#">Listings</a></li>
+        <li><a href="#">Analytics</a></li>
+        <li><a href="#">Profile</a></li>
       </ul>
     </div>
   </template>
   
   <script>
   export default {
-    name: "CustomerNavBar",
+    name: "MerchantNavBar",
   };
   </script>
   
