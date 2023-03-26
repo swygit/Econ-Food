53b592d6eaae8a6ddc2933685ab6e5c3eb73e1c7
Natkann <natthaphon_kanthawang@hotmail.com>
2023-03-26 16:19:25 +0800

    Remove Test file

--- l2g4-econ-food copy/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router/index.js";
-import CustomerNavBar from "./components/CustomerNavBar.vue";
-
-createApp(App)
-  .use(router)
-  .component("CustomerNavBar", CustomerNavBar)
-  .mount("#app");
